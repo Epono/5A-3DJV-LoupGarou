2be1d2c41a7759417a0890fb7ae6341f456aa33b
LeRyokan <tixier.lucas@gmail.com>
2016-05-18 15:40:21 +0200

    Modification du manifest

---
+++ app/src/main/java/fr/esgi/davidghetto/loupgarou/activities/RoleSelectionActivity.java
@@ -0,0 +1,9 @@
+package fr.esgi.davidghetto.loupgarou.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Lucas on 18/05/2016.
+ */
+public class RoleSelectionActivity extends AppCompatActivity {
+}
