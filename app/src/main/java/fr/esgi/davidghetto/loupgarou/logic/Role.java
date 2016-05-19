47d27a4ce1ecdd79290f81933096551444801fb8
Epono <guillaume.ambrois@gmail.com>
2016-05-19 20:11:17 +0200

    Ménage

--- app/src/main/java/fr/esgi/davidghetto/loupgarou/logic/Role.java
+++
@@ -1,5 +0,0 @@
-package fr.esgi.davidghetto.loupgarou.logic;
-
-public enum Role {
-    GAME_MASTER, WEREWOLF, WITCH, MEDIUM, HUNTER, VILLAGER
-}
---
+++ app/src/main/java/fr/esgi/davidghetto/loupgarou/models/Game.java
@@ -0,0 +1,59 @@
+package fr.esgi.davidghetto.loupgarou.models;
+
+import java.util.List;
+
+public class Game {
+    private List<Player> players;
+    private int turn;
+    private int numberOfHumansAlive;
+    private int numberOfWerewolvesAlive;
+
+    public Game(List<Player> players) {
+        this.players = players;
+        this.turn = 0;
+        this.numberOfHumansAlive = 0;
+        this.numberOfWerewolvesAlive = 0;
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public void setTurn(int turn) {
+        this.turn = turn;
+    }
+
+    public int getNumberOfHumansAlive() {
+        return numberOfHumansAlive;
+    }
+
+    public void setNumberOfHumansAlive(int numberOfHumansAlive) {
+        this.numberOfHumansAlive = numberOfHumansAlive;
+    }
+
+    public int getNumberOfWerewolvesAlive() {
+        return numberOfWerewolvesAlive;
+    }
+
+    public void setNumberOfWerewolvesAlive(int numberOfWerewolvesAlive) {
+        this.numberOfWerewolvesAlive = numberOfWerewolvesAlive;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "players=" + players +
+                ", turn=" + turn +
+                ", numberOfHumansAlive=" + numberOfHumansAlive +
+                ", numberOfWerewolvesAlive=" + numberOfWerewolvesAlive +
+                '}';
+    }
+}
